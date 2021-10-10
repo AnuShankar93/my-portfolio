4f781926797f5cf6115592f3e9aec0810abcb350
anushankar93 <anushanakrdivakaran@gmail.com>
2021-10-10 12:32:29 +0530

    Completed menu and header

--- src/features/home/components/header/Header.js
+++ src/features/home/components/header/Header.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import Mobile from './mobile/Mobile';
 import Web from './web/Web';
 
 const Header = () => {
+  const [isOpen,setIsOpen] = useState(false);
     return (
         <div className="header">
           <div className="logo"> Anu Shankar</div>
@@ -12,7 +13,10 @@ const Header = () => {
               <Web/>
             </div>
             <div className="mobile-menu">
-              <Mobile/>
+              <div onClick={()=>setIsOpen(!isOpen)}>
+               <i className="fi fi-rr-align-justify menu-icon"></i>
+              </div>
+              {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen}/>}
             </div>
           </div>
         </div>
--- src/features/home/components/header/mobile/Mobile.js
+++ src/features/home/components/header/mobile/Mobile.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './mobile.css';
 
-const Mobile = () =>  {
+const Mobile = ({ isOpen, setIsOpen }) => {
     return (
-        <div>
-            
+        <div className="mobile">
+            <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+                <i class="fi fi-rr-cross-small" />
+            </div>
+            <div className="mobile-options">
+            <div className="mobile-option">
+                <a href="#project">
+                <i className="fi fi-rr-chart-histogram option-icon"/>Projects
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href="#skills">
+                <i className="fi fi-rr-laptop option-icon"/>Skills
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href="#work">
+                <i className="fi fi-rr-briefcase option-icon"/> Work
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href="#contact">
+                <i class="fi fi-rr-user option-icon"/> Contact
+                </a>
+            </div>
+            </div>
         </div>
     )
 }
