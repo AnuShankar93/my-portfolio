b840de7176c01924b7407d6a91cb23fecd19f075
anushankar93 <anushanakrdivakaran@gmail.com>
2021-10-10 10:58:54 +0530

    Updated the header

--- src/features/home/components/header/Header.js
+++ src/features/home/components/header/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
+import Mobile from './mobile/Mobile';
+import Web from './web/Web';
 
 const Header = () => {
     return (
-        <div>
-          h  
+        <div className="header">
+          <div className="logo"> Anu Shankar</div>
+          <div className="menu">
+            <div className="web-menu">
+              <Web/>
+            </div>
+            <div className="mobile-menu">
+              <Mobile/>
+            </div>
+          </div>
         </div>
     )
 }
---
+++ src/features/home/components/header/mobile/Mobile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './mobile.css';
+
+const Mobile = () =>  {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Mobile;
