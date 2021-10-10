7f0fd7f6d918839ed095eb0a9108ea7635551b6c
anushankar93 <anushanakrdivakaran@gmail.com>
2021-10-10 14:38:08 +0530

    Completed About section

---
+++ src/components/social-contact/data/social.js
@@ -0,0 +1,21 @@
+export const SocialData =[ {
+    platform: 'linkedin',
+    link: 'https://www.linkedin.com/in/anu-shankar-2a3211b3/',
+    icon: require('../../../assets/image/linkedin.png').default,
+},{
+    platform: 'google',
+    link: 'https://www.linkedin.com/in/anu-shankar-2a3211b3/',
+    icon: require('../../../assets/image/google-hangouts.png').default,
+},{
+    platform: 'whatsapp',
+    link: 'https://www.linkedin.com/in/anu-shankar-2a3211b3/',
+    icon: require('../../../assets/image/whatsapp.png').default,
+},{
+    platform: 'github',
+    link: 'https://www.linkedin.com/in/anu-shankar-2a3211b3/',
+    icon: require('../../../assets/image/github.png').default,
+},{
+    platform: 'facebook',
+    link: 'https://www.linkedin.com/in/anu-shankar-2a3211b3/',
+    icon: require('../../../assets/image/facebook.png').default,
+}]
